292430ce4edae98b10ffd660643137ac8c7d5fc6
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-19 16:17:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Your code here
 
 function createEmployeeRecord(firstName,familyName,title,payPerHour,timeInEvents,timeOutEvents){
-  let array=[]
-  array={
-    firstName, 
-    familyName, 
-    title,
-    payPerHour,
-    timeInEvents,
-    timeOutEvents,
-  };
-return array;
-
-}
+    let array=[]
+    array=[
+      firstName, 
+      familyName, 
+      title,
+      payPerHour,
+      timeInEvents,
+      timeOutEvents,
+    ];
+  return array;
+  
+  }
