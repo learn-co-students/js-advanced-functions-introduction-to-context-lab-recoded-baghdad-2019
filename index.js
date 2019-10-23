f9db8eee101935f08ce1ccb6e0694117f585414e
ibraheem Hasan <rules656@gmail.com>
2019-10-23 20:34:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function createEmployeeRecord(emprec){
+  let arr = {
+    "firstName": emprec[0],
+    "familyName": emprec[1],
+    "title": emprec[2],
+    "payPerHour": emprec[3],
+    "timeInEvents": [] ,
+    "timeOutEvents": [],
+  };
+return arr}
+
+function createEmployees(employees){
+  let newEmployeearr = []
+  for(let i = 0; i<employees.length;i++){
+    newEmployeearr.push(createEmployeeRecord(employees[i]));
+  }
+return newEmployeearr}
+
+function createTimeInEvent(emprec,timeIn){
+     let [date, hour] = timeIn.split(' ');
+     emprec.timeInEvents.push({
+       type: "TimeIn",
+       hour:parseInt(hour,10),
+       date
+     }) 
+     return emprec
+}
